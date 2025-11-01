7f986ea5887e67ba1ae58babeaaff1c8e9673d11
bocaj111004 <bocaj111004@gmail.com>
2025-11-01 15:38:37 +0000

    Implement sound notification functionality
    Added a function to play notification sounds.

--- Components/Notifications.lua
+++ Components/Notifications.lua
@@ -4,6 +4,16 @@ return {
     if OptionsTable.Reason then
       OptionsTable.Reason = "\n" .. OptionsTable.Reason
   end
+Sound = function()
+	local NewSound = Instance.new("Sound")
+	NewSound.Volume = Options.NotificationVolume.Value
+	NewSound.SoundId = Options.NotificationSound.Value
+	NewSound:Play()
+	NewSound.Parent = game.ReplicatedStorage
+	NewSound.Ended:Connect(function()
+	NewSound:Destroy()
+	end)
+end
     
   Library:Notify("<b>[" .. OptionsTable.Title .. "]<\b>\n" .. OptionsTable.Description .. OptionsTable.Reason, Time, AbysallHubCore.NotifcationSound)
   end
