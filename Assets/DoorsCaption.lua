86d19e978ccd35e49c77db6d1d1de2691a19878d
bocaj111004 <bocaj111004@gmail.com>
2025-11-01 15:41:13 +0000

    Fix indentation for variable declarations

--- Assets/DoorsCaption.lua
+++ Assets/DoorsCaption.lua
@@ -1,6 +1,6 @@
 return function(Text, PlaySound)
-  local LocalPlayer = game:GetService("Players").LocalPlayer
-local TweenService = game:GetService("TweenService")
+        local LocalPlayer = game:GetService("Players").LocalPlayer
+        local TweenService = game:GetService("TweenService")
 		local MainUI = LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("MainUI")
 		local Caption = MainUI:WaitForChild("MainFrame"):WaitForChild("Caption"):Clone()
 		local CaptionSound = MainUI:WaitForChild("Initiator"):WaitForChild("Main_Game"):WaitForChild("Reminder"):WaitForChild("Caption")
