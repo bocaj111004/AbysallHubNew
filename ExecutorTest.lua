b63c38c1a9a8416fb4bd7e348a930c6007e3edf7
bocaj111004 <bocaj111004@gmail.com>
2025-12-31 15:49:02 +0000

    Create ExecutorTest.lua

---
+++ ExecutorTest.lua
@@ -0,0 +1,2 @@
+-- i made this to fix a friend's old script
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/bocaj111004/AbysallHub/refs/heads/main/Components/ExecutorSupport.lua"))()
